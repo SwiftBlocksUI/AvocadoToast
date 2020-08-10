4f8f13f6512aa4ea1dcd70b43e6d1169af74e5d1
Helge Heß <devteam@zeezide.de>
2020-08-10 17:53:05 +0200

    Bump BlocksUI dep to 0.8.3
    ... only the latest is the greatest.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   
   dependencies: [
     .package(url: "https://github.com/SwiftBlocksUI/SwiftBlocksUI.git",
-             from: "0.8.0")
+             from: "0.8.3")
   ],
   
   targets: [
